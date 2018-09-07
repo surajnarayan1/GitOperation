c25f82b63caf46e7d0acf4bdafde819c3ef443c5
SurajNarayan <snarayan@tavisca.com>
2018-09-07 12:01:07 +0530

    Added teh start file with Practical Implemenatation failure

---
+++ PracticalInplemention/GitOperation/Start.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GitOperation
+{
+    public class Start
+    {
+
+        public int operation(int a ,int b)
+         {
+            int sum = a + b;
+            int diff = a - b;
+            int div = a / b;
+            int mult = a * b;
+            return 0;
+        }
+        
+    }
+}
