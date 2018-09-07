c5ea773209e6db3a8040375ceec957df39df4623
SurajNarayan <snarayan@tavisca.com>
2018-09-07 12:18:53 +0530

    Added the class file for develop

---
+++ PracticalInplemention/GitOperation/ClassforDevelop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitOperation
+{
+   public class ClassforDevelop
+    {
+
+        public String PrintNormalStatement()
+        {
+            String tittle = "Mr.";
+            String FirstName = "Test";
+            String LastName = "123!@#";
+            return  tittle + FirstName + LastName;
+           
+
+        }
+
+       
+    }
+}
